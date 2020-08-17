3d05e1bfa371b76b77d68c2fc0ba52f61642d891
Peyton Clark <42493169+Pclark08@users.noreply.github.com>
2020-08-16 22:19:55 -0400

    removed map(), the script is able to calculate tax

--- caltax.py
+++ caltax.py
@@ -1,18 +1,30 @@
+import pprint 
+
 """Based on maryland tax laws"""
-txa = []
-stand_tax_rate = 0.6
+txas =  []   
+stand_tax_rate = 0.06
 
 print("What is the price of the item?:")
-t = float(input()) 
+tax = float(input()) 
 
-txa.append(t)
+#add user input to a list 
+
+#txas.append(t)
   
-print(txa)
+#print(txas)
 
-def get_price_tax(txa):
-	return txa * (1 + stand_tax_rate)
-	print(stand_tax_rate)
-final = map(get_price_tax, txa)
-final_price = list(final)
-print(final)
+def get_price_tax(t):
+	return t * (1 + stand_tax_rate)
+
+
+final_price = get_price_tax(tax)
+
+print(final_price)
+
+
+
+
+#print ( map(et_price_tax,  txas))
+#final_price = list(final)
+#print (final)	
 
